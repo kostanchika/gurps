c17aa6f08450e4a8c1cb2019da476f4d67cb05e8
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-04-08 00:00:57 +0300

    feat: jwt TokenService

---
+++ src/UsersService/UsersService.Infrastructure/Services/JWTTokenService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using UsersService.Application.Interfaces.Services;
+using UsersService.Infrastructure.Services.Configurations;
+
+namespace UsersService.Infrastructure.Services
+{
+    public class JWTTokenService : ITokenService
+    {
+        private readonly JWTSettings _jwtSettings;
+
+        public JWTTokenService(IOptions<JWTSettings> jwtSettings)
+        {
+            _jwtSettings = jwtSettings.Value;
+        }
+
+        public string GenerateAccessToken(int userId, string login, string role)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId.ToString()),
+                new Claim(ClaimTypes.Name, login),
+                new Claim(ClaimTypes.Role, role),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtSettings.SecretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _jwtSettings.Issuer,
+                audience: _jwtSettings.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(_jwtSettings.AccessTokenExpirationMinutes),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
--- src/UsersService/UsersService.Presentation/Program.cs
+++ src/UsersService/UsersService.Presentation/Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using UsersService.Application.Interfaces.Services;
 using UsersService.Domain.Entities;
 using UsersService.Domain.Interfaces;
 using UsersService.Infrastructure.Persistence.SQL;
@@ -8,6 +12,8 @@ using FluentValidation.AspNetCore;
 using FluentValidation;
 using UsersService.Application.Validators.Auth;
 using UsersService.Presentation.Middlewares;
+using UsersService.Infrastructure.Services;
+using UsersService.Infrastructure.Services.Configurations;
 
 namespace UsersService.Presentation
 {
@@ -23,6 +29,9 @@ namespace UsersService.Presentation
             builder.Services.AddScoped<IRepository<UserEntity>, Repository<UserEntity>>();
             builder.Services.AddScoped<IRepository<FriendshipEntity>, Repository<FriendshipEntity>>();
 
+            // Settings
+            builder.Services.Configure<JWTSettings>(builder.Configuration.GetSection("JWT"));
+
             // Validation
             builder.Services.AddFluentValidationAutoValidation();
             builder.Services.AddValidatorsFromAssemblyContaining(typeof(RegisterValidator));
@@ -35,15 +44,39 @@ namespace UsersService.Presentation
             builder.Services.AddScoped<IRegisterUseCase, RegisterUseCase>();
             builder.Services.AddScoped<IResetPasswordUseCase, ResetPasswordUseCase>();
 
+            // Services
+            builder.Services.AddScoped<ITokenService, JWTTokenService>();
             // Middlewares
             builder.Services.AddScoped<ExceptionHandlingMiddleware>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    var jwtSettings = builder.Configuration.GetSection("JWT").Get<JWTSettings>()
+                        ?? throw new Exception("JWT section is missing or bad configured");
+
+                    options.TokenValidationParameters = new()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings.SecretKey))
+                    };
+                });
 
             var app = builder.Build();
 
             app.UseMiddleware<ExceptionHandlingMiddleware>();
 
+            app.UseAuthentication();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
