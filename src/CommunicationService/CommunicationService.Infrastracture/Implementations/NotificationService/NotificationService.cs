81327d2baab0d90988be6d8e605d8747aa5b241f
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-06-05 00:41:30 +0300

    feat: rabbit mq consumer for notifications

---
+++ src/CommunicationService/CommunicationService.Infrastracture/Implementations/NotificationService/NotificationService.cs
@@ -0,0 +1,23 @@
+﻿using CommunicationService.Application.Dto.Notification;
+using CommunicationService.Application.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CommunicationService.Infrastracture.Implementations.NotificationService
+{
+    public class NotificationService : INotificationService
+    {
+        private readonly IHubContext<NotificationHub> _hubContext;
+
+        public NotificationService(IHubContext<NotificationHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+        public async Task NotifyNotificationSent(NotificationDto notificationDto, CancellationToken cancellationToken = default)
+        {
+            await _hubContext.Clients.User(notificationDto.UserLogin)
+                .SendAsync("RecieveNotification", notificationDto, cancellationToken: cancellationToken);
+        }
+    }
+}
