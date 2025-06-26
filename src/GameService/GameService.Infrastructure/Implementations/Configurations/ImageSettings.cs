a185f3c535e74f864f4b1cfc1dbe55e386ba9e4f
Konstantin Baranchuk <kos5anchik@gmail.com>
2025-06-26 19:30:35 +0300

    feat: game's implementations

---
+++ src/GameService/GameService.Infrastructure/Implementations/Configurations/ImageSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace GameService.Infrastructure.Implementations.Configurations
+{
+    public class ImageSettings
+    {
+        public string ImagesDirectory { get; set; } = "Images";
+    }
+}
